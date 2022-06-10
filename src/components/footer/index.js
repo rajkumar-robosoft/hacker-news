ca29ad6ddf94b1d0aeb4984dec1597ba1bdd3703
“rajkumar-robosoft” <rajkumar.singh@robosoftin.com>
2022-06-10 11:30:40 +0530

    Updated Footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,27 +1,43 @@
-import './style.css';
+import "./style.css";
 
 const Footer = () => {
-    return (
-        <div className='footer'>
-
-                <div className="footer-menu">
-                <a href="https://news.ycombinator.com/newsguidelines.html">Guidelines</a>
-                <a href="https://news.ycombinator.com/newsfaq.html">FAQ</a>
-                <a href="https://news.ycombinator.com/lists">Lists</a>
-                <a href="https://github.com/HackerNews/API">API</a>
-                <a href="https://news.ycombinator.com/security.html">Security</a>
-                <a href="http://www.ycombinator.com/legal/">Legal</a>
-                <a href="http://www.ycombinator.com/apply/">Apply to YC</a>
-                <a href="mailto:hn@ycombinator.com">Contact</a>
-                </div>
-               <div className='footer-search'>
-               <form method="get" >
-                    Search: <input type="text" name="query" />
-                </form>
-               </div>
-                
-          
-        </div>
-    )
-}
+  return (
+    <div className="footer">
+      <div className="footer-menu">
+        <a
+          href="https://news.ycombinator.com/newsguidelines.html"
+          target="_blank"
+        >
+          Guidelines
+        </a>
+        <a href="https://news.ycombinator.com/newsfaq.html" target="_blank">
+          FAQ
+        </a>
+        <a href="https://news.ycombinator.com/lists" target="_blank">
+          Lists
+        </a>
+        <a href="https://github.com/HackerNews/API" target="_blank">
+          API
+        </a>
+        <a href="https://news.ycombinator.com/security.html" target="_blank">
+          Security
+        </a>
+        <a href="http://www.ycombinator.com/legal/" target="_blank">
+          Legal
+        </a>
+        <a href="http://www.ycombinator.com/apply/" target="_blank">
+          Apply to YC
+        </a>
+        <a href="mailto:hn@ycombinator.com" target="_blank">
+          Contact
+        </a>
+      </div>
+      <div className="footer-search">
+        <form method="get">
+          Search: <input type="text" name="query" />
+        </form>
+      </div>
+    </div>
+  );
+};
 export default Footer;
